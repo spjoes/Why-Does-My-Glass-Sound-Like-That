a6153d584b59266862ef31c824ab9de4468c85ab
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2023-09-04 14:04:34 -0400

    Init 1.19

--- src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
+++ src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
@@ -1,8 +1,8 @@
 package com.split.whydoesmyglasssoundlikethat.mixin;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -14,12 +14,13 @@ import org.json.*;
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
     @Unique
-    private static boolean firstRenderCompleted = false;
+    private boolean firstRenderCompleted = false;
     @Inject(method = "render", at = @At("HEAD"))
-    private void onFirstRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void onFirstRender(MatrixStack matrixStack, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (firstRenderCompleted) return;
+        firstRenderCompleted = true;
         //get json from url and check if version is latest
-        String url = "https://raw.githubusercontent.com/spjoes/Why-Does-My-Glass-Sound-Like-That/1.20/update.json";
+        String url = "https://api.persn.dev/api/warden/getModFiles/wdmgslt/update.json";
         String json = "";
         try {
             json = new java.util.Scanner(new java.net.URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
@@ -31,7 +32,6 @@ public class TitleScreenMixin {
         String latestVersion = obj.getString("latestVersion");
         String currentVersion = FabricLoader.getInstance().getModContainer("whydoesmyglasssoundlikethat").get().getMetadata().getVersion().getFriendlyString();
         if (currentVersion.equals(latestVersion)) return;
-        firstRenderCompleted = true;
         SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT, Text.of(obj.getString("updateTitle")), Text.of(obj.getString("updateMessage")));
         MinecraftClient.getInstance().getToastManager().add(toast);
     }
