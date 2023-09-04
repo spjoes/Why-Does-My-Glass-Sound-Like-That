0008859b3eeadb2511e5bd43bb4eb063de64fd6a
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2023-09-04 14:18:44 -0400

    1.18 init

--- src/main/java/com/split/whydoesmyglasssoundlikethat/WDMGSLT.java
+++ src/main/java/com/split/whydoesmyglasssoundlikethat/WDMGSLT.java
@@ -1,9 +1,10 @@
 package com.split.whydoesmyglasssoundlikethat;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.tag.TagKey;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -11,7 +12,8 @@ import net.minecraft.util.registry.RegistryKey;
 public class WDMGSLT implements ModInitializer {
     public static final Identifier SILKTOUCH_BREAK_ID = new Identifier("whydoesmyglasssoundlikethat:silktouch_glass_break");
     public static SoundEvent BLOCK_SILKTOUCH_BREAK = new SoundEvent(SILKTOUCH_BREAK_ID);
-    public static final TagKey<Block> SilkTouchGoodSound = TagKey.of(Registry.BLOCK_KEY, new Identifier("whydoesmyglasssoundlikethat", "silk_touch_good_sound"));
+    public static final Tag<Block> SilkTouchGoodSound = TagFactory.BLOCK.create(new Identifier("whydoesmyglasssoundlikethat", "silk_touch_good_sound"));
+
     @Override
     public void onInitialize() {
         System.out.println("Why Does My Glass Sound Like That has been initialized!");
