f2b88e91348e9ebd1d78ef125ab7cbce6920fe01
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2023-09-04 14:26:31 -0400

    init 1.19.3-1.19.4

--- src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
+++ src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
@@ -1,8 +1,8 @@
 package com.split.whydoesmyglasssoundlikethat.mixin;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.toast.SystemToast;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,7 +16,7 @@ public class TitleScreenMixin {
     @Unique
     private static boolean firstRenderCompleted = false;
     @Inject(method = "render", at = @At("HEAD"))
-    private void onFirstRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void onFirstRender(MatrixStack stack, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (firstRenderCompleted) return;
         firstRenderCompleted = true;
         //get json from url and check if version is latest
