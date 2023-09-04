78cb1f731f0e7296b66d5bcd74c31a560ce288c8
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2023-09-04 14:05:44 -0400

    Fixed lag and changed update url

--- src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
+++ src/main/java/com/split/whydoesmyglasssoundlikethat/mixin/TitleScreenMixin.java
@@ -18,8 +18,9 @@ public class TitleScreenMixin {
     @Inject(method = "render", at = @At("HEAD"))
     private void onFirstRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (firstRenderCompleted) return;
+        firstRenderCompleted = true;
         //get json from url and check if version is latest
-        String url = "https://raw.githubusercontent.com/spjoes/Why-Does-My-Glass-Sound-Like-That/1.20/update.json";
+        String url = "https://api.persn.dev/api/warden/getModFiles/wdmgslt/update.json";
         String json = "";
         try {
             json = new java.util.Scanner(new java.net.URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
@@ -31,7 +32,6 @@ public class TitleScreenMixin {
         String latestVersion = obj.getString("latestVersion");
         String currentVersion = FabricLoader.getInstance().getModContainer("whydoesmyglasssoundlikethat").get().getMetadata().getVersion().getFriendlyString();
         if (currentVersion.equals(latestVersion)) return;
-        firstRenderCompleted = true;
         SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT, Text.of(obj.getString("updateTitle")), Text.of(obj.getString("updateMessage")));
         MinecraftClient.getInstance().getToastManager().add(toast);
     }
